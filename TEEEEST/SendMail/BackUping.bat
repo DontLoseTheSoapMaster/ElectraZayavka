b12c28b1c723fb466000a731864ff506d5e15753
DontLoseTheSoapMaster <53578677+DontLoseTheSoapMaster@users.noreply.github.com>
2021-08-06 12:40:54 +0300

    BaseFiles
    This is files That working together.
    Main file is "Электронная заявка на пропуск". Base is - "SystemEZP". Cheblons - contain Print templates. "Инструкции" - Contain Images for instructions, that used in the Programm. "Sounds" contain Sounds that use the programm. SendMail Contain files that uses for sending mail to Head of the Pass Office. Archive contain an archive database

---
+++ TEEEEST/SendMail/BackUping.bat
@@ -0,0 +1,3 @@
+cd C:\Program Files\7-Zip
+7z a -mx1 C:\Users\bocharova\Desktop\ArchiveBase
+C:\Users\bocharova\Desktop\TEEEEST - 4
---
+++ TEEEEST/SendMail/Send!.bat
@@ -0,0 +1,41 @@
+SET TARGETPATH=%~dp0sendmail.ini
+IF NOT EXIST "%TARGETPATH%" GOTO :ERROR
+SET TARGETPATH=%~dp0sendmail.exe
+IF NOT EXIST "%TARGETPATH%" GOTO :ERROR
+SET TARGETPATH=%~dp0Message.txt
+IF NOT EXIST "%TARGETPATH%" GOTO :ERROR
+SET TARGETPATH=%~dp0libeay32.dll
+IF NOT EXIST "%TARGETPATH%" GOTO :ERROR
+SET TARGETPATH=%~dp0ssleay32.dll
+IF NOT EXIST "%TARGETPATH%" GOTO :ERROR
+SET TARGETPATH=%~dp0Message.txt
+"%~dp0sendmail.exe"   -t   0<  "%TARGETPATH%"
+erase "%~dp0debug.log"
+SET TARGETPATH=%~dp0error.log
+IF NOT EXIST "%TARGETPATH%" GOTO :ERROR2
+erase "%~dp0crash.txt"
+erase "%~dp0error.log"
+SET msgboxTitle=PISMO NE OTPRAVLENO!
+SET msgboxBody=PISMO NE OTPRAVLENO!
+SET tmpmsgbox=%temp%\~tmpmsgbox.vbs
+IF EXIST "%tmpmsgbox%" DEL /F /Q "%tmpmsgbox%"
+ECHO msgbox "%msgboxBody%",0,"%msgboxTitle%">"%tmpmsgbox%"
+WSCRIPT "%tmpmsgbox%"
+GOTO :END
+:ERROR
+SET msgboxTitle=PISMO NE OTPRAVLENO!
+SET msgboxBody=PISMO NE OTPRAVLENO!
+SET tmpmsgbox=%temp%\~tmpmsgbox.vbs
+IF EXIST "%tmpmsgbox%" DEL /F /Q "%tmpmsgbox%"
+ECHO msgbox "%msgboxBody%",0,"%msgboxTitle%">"%tmpmsgbox%"
+WSCRIPT "%tmpmsgbox%"
+GOTO :END
+:ERROR2
+SET msgboxTitle=Pismo Otpravleno!
+SET msgboxBody=Pismo Otpravleno!
+SET tmpmsgbox=%temp%\~tmpmsgbox.vbs
+IF EXIST "%tmpmsgbox%" DEL /F /Q "%tmpmsgbox%"
+ECHO msgbox "%msgboxBody%",0,"%msgboxTitle%">"%tmpmsgbox%"
+WSCRIPT "%tmpmsgbox%"
+SET msgboxBody=This is my Message Body
+:END
